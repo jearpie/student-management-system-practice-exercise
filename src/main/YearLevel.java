7e4680aadecedcf8fce2f3cefc50a4ec6a4f9a17
Johnrey Aborot <johnreyaborot10@gmail.com>
2026-05-28 15:01:41 +0800

    Added 5th Year

--- src/main/YearLevel.java
+++ src/main/YearLevel.java
@@ -4,16 +4,18 @@ public enum YearLevel {
     FIRST_YEAR,
     SECOND_YEAR,
     THIRD_YEAR,
-    FOURTH_YEAR;
+    FOURTH_YEAR,
+    FIFTH_YEAR; // Added 5th year constant
 
     @Override
     public String toString() {
-        return switch (this) {
-            case FIRST_YEAR -> "1st Year";
-            case SECOND_YEAR -> "2nd Year";
-            case THIRD_YEAR -> "3rd Year";
-            case FOURTH_YEAR -> "4th Year";
-            default -> "";
-        };
+        switch (this) {
+            case FIRST_YEAR: return "1st Year";
+            case SECOND_YEAR: return "2nd Year";
+            case THIRD_YEAR: return "3rd Year";
+            case FOURTH_YEAR: return "4th Year";
+            case FIFTH_YEAR: return "5th Year"; // Added formatted text
+            default: return "";
+        }
     }
 }
